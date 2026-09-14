30d02fb1072e88039ce37d6b92100162c9916cab
Lily Shih <lilyshih@Lilys-MacBook-Air.local>
2026-09-13 17:39:29 -0700

    scaffold Dagster project, wire dbt models as assets

---
+++ notif_orchestration/notif_orchestration/assets.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from dagster_dbt import DbtCliResource, DbtProject, dbt_assets
+from dagster import AssetExecutionContext
+
+notif_dbt_project = DbtProject(
+    project_dir=Path(__file__).parent.parent.parent / "notif_analytics"
+)
+
+@dbt_asset(manifest=notif_dbt_project.manifest_path)
+def notif_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["build"], context=context).stream()
+    
+
+
